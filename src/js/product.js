4cb3c1c0331e01a635fdcc2066c6a30632a8084d
nomech <mech1803@gmail.com>
2024-11-25 00:03:26 +0100

    Refactor product details display; implement product view functionality and improve layout for better user interaction

--- src/js/product.js
+++ src/js/product.js
@@ -7,42 +7,79 @@ const getProductObject = () => {
   return category;
 };
 
-const products = document.querySelector(".products")
+const products = document.querySelector(".products");
+
+const viewProduct = (event) => {
+  event.preventDefault();
+  const id = event.target.parentNode.dataset.id;
+
+  const product = getProductObject().items.find(
+    (category) => category.id == id
+  );
+  const products = document.querySelector(".products");
+  products.classList.toggle("products--show");
+
+  const details = document.querySelector(".details");
+  details.classList.toggle("details--show");
+
+  const detailsImage = document.querySelector(".details__image");
+  detailsImage.src = product.image;
+
+  const productPrice = document.querySelector(".details__price");
+  productPrice.innerText = `$ ${product.price}`;
+
+  const detailsText = document.querySelector(".details__text");
+  detailsText.innerText = product.description;
+
+  const detailsList = document.querySelector(".details__list");
+  
+  for (let spec in product.spec) {
+    const specItem = document.createElement("li");
+    specItem.innerText = `${spec}: ${product.spec[spec]}`;
+
+    detailsList.append(specItem);
+  }
+
+  const orderButton = document.createElement("button");
+  orderButton.classList.add("details__order", "button", "button--order");
+  orderButton.innerText = "Order";
+
+  detailsList.append(orderButton);
+};
 
 const createProductItems = () => {
-    const category = getProductObject();
+  const category = getProductObject();
 
-    category.items.forEach(item => {
-        const product = document.createElement("div");
-        product.classList.add("products__item");
+  category.items.forEach((item) => {
+    const product = document.createElement("div");
+    product.classList.add("products__item");
 
-        const productImage = document.createElement("img");
-        productImage.classList.add("products__image")
-        productImage.src = item.image;
+    product.dataset.id = item.id;
 
-        const textGroup = document.createElement("div")
-        textGroup.classList.add("products__text-group")
+    const productImage = document.createElement("img");
+    productImage.classList.add("products__image");
+    productImage.src = item.image;
 
-        const productPrice = document.createElement("p")
-        productPrice.classList.add("products__price")
-        productPrice.innerText = item.price;
+    const textGroup = document.createElement("div");
+    textGroup.classList.add("products__text-group");
 
-        const productName = document.createElement("h2");
-        productName.classList.add("products__name");
-        productName.innerText = item.name;
+    const productPrice = document.createElement("p");
+    productPrice.classList.add("products__price");
+    productPrice.innerText = item.price;
 
-        const viewButton = document.createElement("button")
-        viewButton.classList.add("products__view", "button", "button--view")
-        viewButton.innerText = "View"
-        viewButton.addEventListener("click", () => {
+    const productName = document.createElement("h2");
+    productName.classList.add("products__name");
+    productName.innerText = item.name;
 
-        })
+    const viewButton = document.createElement("button");
+    viewButton.classList.add("products__view", "button", "button--view");
+    viewButton.innerText = "View";
+    viewButton.addEventListener("click", (event) => viewProduct(event));
 
-        textGroup.append(productName, productPrice)
-        product.append(productImage, textGroup, viewButton)
-        products.append(product)
+    textGroup.append(productName, productPrice);
+    product.append(productImage, textGroup, viewButton);
+    products.append(product);
+  });
+};
 
-    })
-}
-
-createProductItems()
+createProductItems();
